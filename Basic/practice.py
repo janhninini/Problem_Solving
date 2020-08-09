5bd61bd4ee7031e83d8b8daa2fc9f8cdbe43abed
janhvi <janhvi1009@gmail.com>
2020-08-09 23:03:39 +0530

    new questions

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,21 +1,6 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    if (n%2!=0):
-        print("Weird")
-        
-    elif(n==2 or n==4):
-        print("Not Weird")
-    elif(n>=6 and n<=20):
-        print("Weird")
-    else:
-        print("Not Weird")
+from itertools import permutations
+for i in range(int(input())):
+    s = input()
+    p = input()
+    for i in permutations(s):
+        print ["".join(i)]
