b3c8fa6b9598882426000c1474bd8e1dc4cf5f38
janhvi <janhvi1009@gmail.com>
2020-08-18 20:59:20 +0530

    2 new questions

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,6 +1,8 @@
-from itertools import permutations
-for i in range(int(input())):
-    s = input()
-    p = input()
-    for i in permutations(s):
-        print ["".join(i)]
+n = int(input())
+a = list(map(int,input().split()))
+nv = {"zero": 2, "one":2, "two":1, "three": 3, "four": 2, "five":2, "six":1, "seven": 2, "eight":2, "nine":2}
+D = 0
+for i in range(n):
+    for j in str(a[i]):
+        D = D + 
+
