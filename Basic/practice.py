39fef871c4bb7b0d13e7aa78eaa339f0ed8a1b98
janhvi <janhvi1009@gmail.com>
2020-10-25 16:44:12 +0530

    new problem

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,8 +1,2 @@
-n = int(input())
-a = list(map(int,input().split()))
-nv = {"zero": 2, "one":2, "two":1, "three": 3, "four": 2, "five":2, "six":1, "seven": 2, "eight":2, "nine":2}
-D = 0
-for i in range(n):
-    for j in str(a[i]):
-        D = D + 
+
 
